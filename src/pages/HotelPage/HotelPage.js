dc6714cc1384232d22373a50c7c01e781315abe1
Benjamin Gauvin <m.kaerin@gmail.com>
2024-01-12 20:22:32 +0100

    début du caroussel

--- src/pages/HotelPage/HotelPage.js
+++ src/pages/HotelPage/HotelPage.js
@@ -2,19 +2,33 @@ import { useLocation } from "react-router-dom";
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
 import hotelList from '../../annonces.json'
+import PageError from '../PageError/PageError'
 
 function HotelPage() {
     let currentLink = useLocation()
     let hotelId = currentLink.pathname.slice(1)
     console.log(hotelId)
 
-    //il faut checker si hotelList contient hotelID, si oui, afficher la page
-    //sinon afficher PageError
-
-    return<>
+    const hotelFound = hotelList.find( obj => {
+        return obj.id === hotelId
+    })
+    console.log(hotelFound)
+    //reprendre le système de caroussel pour charger une image à la
+    //fois en se baladant dans la liste des images.
+    const carousselPics = hotelFound.pictures.map((picLink) => 
+        <img src={picLink} alt={picLink.split("project/").pop()} className="caroussel__image"/>
+    )
+    if (hotelFound) {
+        return<>
     <Header />
+    <section className="caroussel">
+        {carousselPics}
+    </section>
     <Footer />
     </>
+    } else {
+        return <PageError />
+    }
 }
 
 export default HotelPage;
